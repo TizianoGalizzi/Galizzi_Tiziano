e93711d7e5f59857dbfe48084068cd3526dee6ff
Tiziano Galizzi <t.easy213@gmail.com>
2023-01-31 17:38:54 -0300

    TP14 resuelto

---
+++ modulo06/tp14/silicon-tp14/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Menu from './Menu';
+import Home from './Home';
+
+function App() {
+  return (
+  <>
+  <Menu></Menu>
+  <Home></Home>
+
+
+  </>
+  );
+}
+
+export default App;
---
+++ modulo06/tp14/silicon-tp14/src/Home.js
@@ -0,0 +1,13 @@
+
+function Home() {
+    return(
+        <>
+        <div className="container-fluid title">
+            <h1>Esta es mi primer APP react</h1>
+        </div>
+        </>
+
+    );
+};
+
+export default Home;
